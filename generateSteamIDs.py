2e9dc07bc12c9239e75a26835b1c4ff3115c2197
Snuggle <snuggle@sprinkly.net>
2018-08-25 20:14:14 +0100

    Added a few comments.

--- generateSteamIDs.py
+++ generateSteamIDs.py
@@ -8,7 +8,7 @@ OutputPath = 'AppIDs.txt'
 
 
 class SteamPlayLookup:
-    def __init__(self, API_SteamURL, PathList): # This is the main thingy. directly in the constructor
+    def __init__(self, API_SteamURL, PathList): # This is the main script, that gets run first.
         print("Welcome to SteamPlayLookup!\n\nFetching Steam API AppList...")
         SteamAPIList = self.grabJSON(API_SteamURL)
 
@@ -20,24 +20,26 @@ class SteamPlayLookup:
 
         print("Now outputting to your file...")
         OutputFile = open(PathList[1], 'w')
-        for ID in OutputList:
+        for ID in OutputList: # Write each AppID to the output text-file.
             OutputFile.write(f"{ID}\n")
 
         print(f"Finished.")
 
+    # Function to lookup a game's title within the JSON blob from Steam's API.
     def lookup(self, lookup_list, api_json):
         OutputList = []
         for game in lookup_list:
             currentAppID = "NULL" # If cannot be found in API, return "NULL"
             for app in api_json:
-                if app['name'].lower().strip() == game.lower().strip(): # Clean whitespace, case-insensitive
-                    currentAppID = app['appid']
+                if app['name'].lower().strip() == game.lower().strip(): # Clean whitespace, case-insensitive etc.
+                    currentAppID = app['appid'] # If game is found in API, get the appid.
                     break
             print(f"{currentAppID} - {game}")
             OutputList.append(currentAppID)
 
         return OutputList
 
+    # Simple function to grab JSON from a URL. Returns a JSON blob.
     def grabJSON(self, url):
         response = requests.get(url=url)
         data = response.json()
